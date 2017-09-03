dc6e254b255e88172361970405fd0d14cff9b7a5
D8vjork <d8vjork@outlook.com>
2017-09-03 20:23:18 +0200

    New build

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ module.exports = {
     index: verpath + '/index.html',
     assetsRoot: verpath,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/' + ver + '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
